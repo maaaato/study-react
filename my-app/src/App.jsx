32b8ab1a6103fd3fc626bdf594dd93e0b298d6df
maaaato <igosso.mato@gmail.com>
2022-05-12 23:51:27 +0900

    [Update] useState

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  }
+
   return (
     <>
       <h1>hello</h1>
       <ColoredMessage>hoge</ColoredMessage>
       <p>how are you</p>
+      <button onClick={onClickButton}>button</button>
+      <p>{num}</p>
     </>
   );
 };
